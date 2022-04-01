dffd7515e07ee8f37fe567b4a437ee45b83260ae
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2022-04-01 14:58:14 +0100

    Add character count too, for testing tree-shaking

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-import update from './update.js';
 import './index.scss'
 
-import { Accordion } from 'govuk-frontend'
+import { Accordion, CharacterCount } from 'govuk-frontend'
 
 var $accordion = document.querySelector('[data-module="govuk-accordion"]');
 new Accordion($accordion).init();
 
-// even though Rollup is bundling all your files together, errors and
-// logs will still point to your original source modules
-console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
-
-update();
+var $characterCount = document.querySelector('[data-module="govuk-character-count"]')
+new CharacterCount($characterCount).init()
